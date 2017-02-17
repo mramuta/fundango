1b95bbc36bb749961b6b668f273ffaf602665bc3
Matthew Ramuta <matthew.g.ramuta@gmail.com>
2017-02-17 16:03:39 -0600

    add min_to_time method

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -3,5 +3,6 @@ class ShowsController < ApplicationController
 		@shows = Show.all
 		# @showtimes = Show.all.showtimes.where("showtime > ?", Time.now.seconds_since_midnight / 60)
 		# p @showtimes
+		p @shows[1].showtimes[0].min_to_time
 	end
 end
