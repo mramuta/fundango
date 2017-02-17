17edb12fa91b60b6131d5bc2fa866f43e169e4e6
Matthew Ramuta <matthew.g.ramuta@gmail.com>
2017-02-17 13:11:09 -0600

    add orders migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170217184537) do
+ActiveRecord::Schema.define(version: 20170217190326) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 20170217184537) do
     t.integer  "capacity",   null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer  "showtime_id", null: false
+    t.string   "name",        null: false
+    t.string   "email",       null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "shows", force: :cascade do |t|
