b0136dfa6f2078636263d227a7d2914cace89842
Matthew Ramuta <matthew.g.ramuta@gmail.com>
2017-02-17 16:04:17 -0600

    add min_to_time method

--- app/models/showtime.rb
+++ app/models/showtime.rb
@@ -3,5 +3,14 @@ class Showtime < ApplicationRecord
 	belongs_to :auditorium
 	has_many :orders
 	
-	validates :show_id, :auditorium_id, :showtime, presence: true
+	validates :show_id, :auditorium_id, :min_after_midnight, presence: true
+
+	def min_to_time()
+		min = read_attribute(:min_after_midnight)
+		time_str = (min / 60).to_s + ':' + (min % 60).to_s
+		if min % 60 < 10
+			time_str.insert(-2,'0')
+		end
+		time_str
+	end
 end
