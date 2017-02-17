595dda21436c0d76d52b36ad61fa942b9135739b
Matthew Ramuta <matthew.g.ramuta@gmail.com>
2017-02-17 16:04:38 -0600

    modify showtime migration

--- db/migrate/20170217184537_create_showtimes.rb
+++ db/migrate/20170217184537_create_showtimes.rb
@@ -3,7 +3,7 @@ class CreateShowtimes < ActiveRecord::Migration[5.0]
     create_table :showtimes do |t|
 			t.integer :show_id, null: false
       t.integer :auditorium_id, null: false
-      t.integer :showtime, null: false
+      t.integer :min_after_midnight, null: false
       t.timestamps
     end
   end
