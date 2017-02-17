58355e1b4d09bf03e4fd488bf1473ff6af500ddd
Matthew Ramuta <matthew.g.ramuta@gmail.com>
2017-02-17 11:33:31 -0600

    add rspec files

--- spec/models/show_spec.rb
+++ spec/models/show_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe Show, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Show do
+  let(:show) { Show.new }
+
+  describe "validations" do
+    it 'has to have a title, runtime, desc, and image_url' do
+      expect(show.invalid?).to be_truthy
+      show.title = 'Jaws'
+      show.runtime = 99
+      show.description = 'shark eats loads of people'
+      show.image_url = 'www.shark.com'
+      expect(show.invalid?).to be_falsey
+    end
+  end
 end
