c73bdf5e7967a350734dd851532a23f2750940bb
Matthew Ramuta <matthew.g.ramuta@gmail.com>
2017-02-17 16:05:51 -0600

    change showtime model spec to min_after_midnight

--- spec/models/showtime_spec.rb
+++ spec/models/showtime_spec.rb
@@ -10,7 +10,7 @@ describe Showtime do
       expect(showtime.invalid?).to be_truthy
       showtime.auditorium_id = auditorium.id
       showtime.show_id = show.id
-      showtime.showtime = Time.now
+      showtime.min_after_midnight = 1200
       expect(showtime.invalid?).to be_falsey
     end
   end
