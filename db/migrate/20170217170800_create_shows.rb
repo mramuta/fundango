631b1c25e6c210c898f6d380f5f8c77f7014b8ac
Matthew Ramuta <matthew.g.ramuta@gmail.com>
2017-02-17 11:33:05 -0600

    add show migration

--- db/migrate/20170217170800_create_shows.rb
+++ db/migrate/20170217170800_create_shows.rb
@@ -1,8 +1,10 @@
 class CreateShows < ActiveRecord::Migration[5.0]
   def change
     create_table :shows do |t|
-    	has_many :orders
-			has_many :showtimes
+    	t.string :title, null: false
+      t.text :description, null: false
+      t.string :image_url, null: false
+      t.integer :runtime, null: false
       t.timestamps
     end
   end
