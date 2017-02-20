2cb89706aeccaf1609168d33047841ff8f8b6714
Matthew Ramuta <matthew.g.ramuta@gmail.com>
2017-02-20 12:26:11 -0600

    update order spec test

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Order do
+  let(:order) { Order.new }
+
+  describe "validations" do
+    it 'has to have a email, name, and showtime' do
+    	auditorium = Auditorium.create(capacity: 100)
+    	show = Show.create(title: 'Test Show',  description: "test desc", image_url:'test_url', runtime:119)
+    	showtime = Showtime.create(auditorium_id: auditorium.id,show_id: show.id,min_after_midnight: 1200)
+      expect(order.invalid?).to be_truthy
+      order.email = 'test@test.com'
+      order.name = 'test'
+      order.showtime_id = showtime.id
+      expect(order.invalid?).to be_falsey
+    end
+  end
 end
