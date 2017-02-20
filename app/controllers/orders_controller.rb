0f32617bd8d4fdd5c75f48598eae0425fed92bec
Matthew Ramuta <matthew.g.ramuta@gmail.com>
2017-02-20 14:18:04 -0600

    change orders controllers create

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,15 +2,24 @@ class OrdersController < ApplicationController
 	skip_before_action :require_login, only: [:new,:create]
 
   def new
+    @show = Show.find(params[:show_id])
     @order = Order.new
   end
 
   def create
+    @show = Show.find(params[:show_id])
+    @order = Order.new(order_params)
     if @order.save
       redirect_to root_path 
     else
       @errors = @order.errors.full_messages
       render "new"
     end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:name,:email,:showtime_id)
   end
 end
