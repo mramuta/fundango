cd16a3b5a0a8ac6023ec46daee839cdf6cd6b986
Matthew Ramuta <matthew.g.ramuta@gmail.com>
2017-02-20 13:27:37 -0600

    add login checker

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :require_login
+ 	
+ 	def current_user
+    @current_user ||= Admin.find_by(id: session[:admin_id])
+  end
+
+  def logged_in?
+    current_user != nil
+  end
+
+  private
+ 
+  def require_login
+    unless logged_in?
+      redirect_to root_path
+    end
+  end
 end
